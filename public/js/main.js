87f8b2dac6a8d3eec363cf0f8736d6feb62f6fee
Joseph Sungpil Choi <chousemath@gmail.com>
2017-12-22 23:42:37 +0900

    Abstract out some functions

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,23 @@
 'use strict';
 
+var initializeAttribute = function initializeAttribute(name, id) {
+  var attribute = window.localStorage.getItem(name);
+  if (attribute) attribute = parseInt(attribute);else attribute = 0;
+  document.getElementById(id).innerHTML = attribute;
+  return attribute;
+};
+
 var initialize = function initialize() {
   console.log('initialize');
-  window.sessionStorage.setItem('shirtCount', 0);
+  console.log('Value of shirt-count is ' + initializeAttribute('shirtCount', 'shirt-count'));
 };
 
 initialize();
 
 var handleShirtBtnClick = function handleShirtBtnClick() {
-  var shirtCount = window.sessionStorage.getItem('shirtCount');
+  var shirtCount = window.localStorage.getItem('shirtCount');
   if (shirtCount) shirtCount = parseInt(shirtCount);else shirtCount = 0;
-  window.sessionStorage.setItem('shirtCount', shirtCount + 1);
+  window.localStorage.setItem('shirtCount', shirtCount + 1);
   document.getElementById('shirt-count').innerText = shirtCount + 1;
 };
 
@@ -19,3 +26,10 @@ var handlePantsBtnClick = function handlePantsBtnClick() {};
 var handleUnderwearBtnClick = function handleUnderwearBtnClick() {};
 
 var handleSocksBtnClick = function handleSocksBtnClick() {};
+
+var handlePushupsBtnClick = function handlePushupsBtnClick() {
+  var pushupCount = window.localStorage.getItem('pushupCount');
+  if (pushupCount) pushupCount = parseInt(pushupCount);else pushupCount = 0;
+  window.localStorage.setItem('pushupCount', pushupCount + 10);
+  document.getElementById('pushup-count').innerHTML = pushupCount + 10;
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,23 @@
+const initializeAttribute = (name, id) => {
+  let attribute = window.localStorage.getItem(name)
+  if (attribute) attribute = parseInt(attribute)
+  else attribute = 0
+  document.getElementById(id).innerHTML = attribute
+  return attribute
+}
+
 const initialize = () => {
   console.log('initialize')
-  window.sessionStorage.setItem('shirtCount', 0)
+  console.log(`Value of shirt-count is ${initializeAttribute('shirtCount', 'shirt-count')}`)
 }
 
 initialize()
 
 const handleShirtBtnClick = () => {
-  let shirtCount = window.sessionStorage.getItem('shirtCount')
+  let shirtCount = window.localStorage.getItem('shirtCount')
   if (shirtCount) shirtCount = parseInt(shirtCount)
   else shirtCount = 0
-  window.sessionStorage.setItem('shirtCount', shirtCount + 1)
+  window.localStorage.setItem('shirtCount', shirtCount + 1)
   document.getElementById('shirt-count').innerText = (shirtCount + 1)
 }
 
@@ -20,9 +28,9 @@ const handleUnderwearBtnClick = () => {}
 const handleSocksBtnClick = () => {}
 
 const handlePushupsBtnClick = () => {
-  let pushupCount = window.sessionStorage.getItem('pushupCount')
+  let pushupCount = window.localStorage.getItem('pushupCount')
   if (pushupCount) pushupCount = parseInt(pushupCount)
   else pushupCount = 0
-  window.sessionStorage.setItem('pushupCount', pushupCount + 10)
+  window.localStorage.setItem('pushupCount', pushupCount + 10)
   document.getElementById('pushup-count').innerHTML = (pushupCount + 10)
 }
